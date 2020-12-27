dd76458d57f3a28804b38e381084487f3d47683f
Simon Wood <simon@mungewell.org>
2020-12-27 10:19:19 -0700

    Parse structure, content not yet understood

---
+++ decode_pattern.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+# Script decode/encode '.unodrpt' Pattern files from UNO Drum
+# (c) Simon Wood, 26 Dec 2020
+#
+
+from construct import *
+
+#--------------------------------------------------
+# Define UNODRPT file format using Construct (v2.9)
+# requires:
+# https://github.com/construct/construct
+
+BLOB = Struct(
+    "length" / Byte,
+    "blob" / Bytes(this.length),
+)
+
+UNODRPT = Struct(
+    Const(b"UNODrumPattern"),
+    Const(b"\x00\x01\x0d"),
+    Const(b"\x30\x00\x01"), "line0" / BLOB,
+    Const(b"\x31\x00\x01"), "line1" / BLOB,
+    Const(b"\x32\x00\x01"), "line2" / BLOB,
+    Const(b"\x33\x00\x01"), "line3" / BLOB,
+    Const(b"\x34\x00\x01"), "line4" / BLOB,
+    Const(b"\x35\x00\x01"), "line5" / BLOB,
+    Const(b"\x36\x00\x01"), "line6" / BLOB,
+    Const(b"\x37\x00\x01"), "line7" / BLOB,
+    Const(b"\x38\x00\x01"), "line8" / BLOB,
+    Const(b"\x39\x00\x01"), "line9" / BLOB,
+    Const(b"\x31\x30\x00\x01"), "line10" / BLOB,
+    Const(b"\x31\x31\x00\x01"), "line11" / BLOB,
+    Const(b"\x31\x32\x00\x01"), "line12" / BLOB,
+    Const(b"\x00"),
+)
+
+
+#--------------------------------------------------
+def main():
+    from optparse import OptionParser
+
+    usage = "usage: %prog [options] FILENAME"
+    parser = OptionParser(usage)
+    parser.add_option("-d", "--dump",
+        help="dump configuration to text",
+        action="store_true", dest="dump")
+
+    (options, args) = parser.parse_args()
+    
+    if len(args) != 1:
+        parser.error("FILE not specified")
+
+    infile = open(args[0], "rb")
+    if not infile:
+        sys.exit("Unable to open FILE for reading")
+    else:
+        data = infile.read()
+    infile.close()
+
+    if options.dump and data:
+        config = UNODRPT.parse(data)
+        print(config)
+
+
+if __name__ == "__main__":
+    main()
+
